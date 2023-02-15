5c88545f942f93f24b2dfe9ae46639ac916bcba7
cRYP70N <otmane.kimdil@gmail.com>
2023-02-15 20:29:15 +0200

    Initialize deploy

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import cors from 'cors';
+import helmet from 'helmet';
+import express from 'express';
+
+import routes from './routes';
+import ServerlessHttp from 'serverless-http';
+import logHandler from './middlewares/logHandler';
+import notFoundHandler from './middlewares/notFoundHandler';
+import transactionHandler from './middlewares/transactionHandler';
+import genericErrorHandler from './middlewares/genericErrorHandler';
+
+const app: express.Application = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(transactionHandler);
+app.use(logHandler);
+app.use(express.json({ limit: '300kb' }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+app.use(genericErrorHandler);
+app.use(notFoundHandler);
+
+export const handler = ServerlessHttp(app);
+
+export default app;
